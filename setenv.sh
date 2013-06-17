7b9aecce2e96d0d1631aa21fb20503e346261a91
Brian Tingle - DSC role account <dsc@sand1-dev.(none)>
2013-06-17 10:16:03 -0700

    zero out the old shell when you source setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -7,9 +7,21 @@ unset JAVA_ROOT
 unset JDK_HOME
 unset JRE_HOME
 
+# unset build-dsc-solaris10 environment
+unset CFLAGS
+unset CPPFLAGS
+unset LDFLAGS
+unset LD_LIBRARY_PATH
+unset LD_RUN_PATH
+unset PERL5LIB
+unset PREFIX
+# restore default path on SUSE
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/X11R6/bin:/usr/games:/usr/lib/mit/bin:/usr/lib/mit/sbin
+
+
 # don't pick up SUSE's python
 unset PYTHONPATH
 unset PYTHONSTARTUP
 
-export PATH=${HOME}/pkg/bin:${HOME}/pkg/sbin:${JAVA_HOME}/bin:${PATH}
+export PATH=${HOME}/pkg/bin:${HOME}/pkg/sbin:${HOME}/pkg/lib/perl5/site_perl/bin:${JAVA_HOME}/bin:${PATH}
 export MANPATH=${HOME}/pkg/man
