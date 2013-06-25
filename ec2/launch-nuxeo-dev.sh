545b0742b1a26d2d025c3286316d7f496eb7e6e6
Mark Redar <mredar@gmail.com>
2013-06-25 14:50:06 -0700

    Made base_user_data.sh minimal. For appstrap provisioned boxes.

--- ec2/launch-nuxeo-dev.sh
+++ ec2/launch-nuxeo-dev.sh
@@ -3,9 +3,15 @@ AMI_EBS="ami-1624987f"
 EC2_SIZE="t1.micro"
 ZONE=us-east-1b
 
+cat base_user_data.sh > nuxeo_ec2_init.sh
+
+cat >> nuxeo_ec2_init.sh << DELIM
+useradd nuxeo
+DELIM
+
 ec2-run-instances $AMI_EBS          \
      --verbose                        \
-     --user-data-file base_user_data.sh  \
+     --user-data-file nuxeo_ec2_init.sh  \
      --key majorTom-keypair                \
      --monitor                        \
      --instance-type $EC2_SIZE         \
