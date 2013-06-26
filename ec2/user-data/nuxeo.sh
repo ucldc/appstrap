00ee67e77db711024f362272e80a52b7aeab85a2
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2013-06-26 05:44:35 +0000

    upgrade from set -eu
    if [[ -n "$DEBUG" ]]; then

--- ec2/user-data/nuxeo.sh
+++ ec2/user-data/nuxeo.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # cloudinit script
 # https://help.ubuntu.com/community/CloudInit
+set -o pipefail  # trace ERR through pipes
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 yum -y update    # for security
 yum -y install git
 yum -y groupinstall "Development Tools"
