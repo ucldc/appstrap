34192b7ff4462fd91cbe12242a9890bcf1672ec3
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2013-07-23 19:55:43 -0700

    registry stack with the new shibboleth build working; ansible next

--- setenv.sh
+++ setenv.sh
@@ -39,3 +39,4 @@ unset PYTHONSTARTUP
 
 export PATH=${HOME}/pkg/bin:${HOME}/pkg/sbin:${HOME}/pkg/lib/perl5/site_perl/bin:${JAVA_HOME}/bin:${PATH}
 export MANPATH=${HOME}/pkg/man:/usr/local/man:/usr/local/share/man:/usr/share/man
+export LD_LIBRARY_PATH=${HOME}/pkg/lib:${HOME}/servers/shibboleth/lib
