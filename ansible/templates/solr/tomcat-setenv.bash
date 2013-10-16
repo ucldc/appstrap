e15747755b3d88c6ccfff48c09c6e22c3781b505
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2013-10-16 09:29:16 -0700

    Update for new jpache style install and solr 4.5.
    May need to back out solr4.5 stuff.

--- ansible/templates/solr/tomcat-setenv.bash
+++ ansible/templates/solr/tomcat-setenv.bash
@@ -1,4 +1,4 @@
-export CATALINA_HOME=${HOME}/java/tomcat
+export CATALINA_HOME=${HOME}/java/apache-tomcat
 export CATALINA_BASE=${HOME}/servers/tomcat
 export CATALINA_PID=${HOME}/servers/tomcat/tomcat.pid
 export CATALINA_OPTS="-Dsolr.solr.home=${HOME}/servers/solr"
